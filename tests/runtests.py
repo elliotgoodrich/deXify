0c087f29011bb66b046e82db272920270108c12c
Elliot Goodrich <elliotgoodrich@gmail.com>
2011-05-07 20:07:27 +0100

    Updated the test script to ignore tests with no expected output

--- tests/runtests.py
+++ tests/runtests.py
@@ -1,7 +1,7 @@
 # Title: runtests.py
 # Author: Elliot Goodrich <http://elliotgoodri.ch/>
 # License: CC0 Public Domain Dedication <http://creativecommons.org/publicdomain/zero/1.0/>
-# Description: A script to test the XSLT file against input XML files and expected HTML files
+# Description: A script to test deXify against input XML files and expected HTML files
 
 import os
 import re
@@ -13,8 +13,9 @@ input_dir = "input"
 output_dir = "output"
 deXify_file = "../deXify.xslt"
 
-# test counter
-test_count = 0
+# test counters
+passed_tests = 0
+skipped_tests = 0
 
 # load the regex to get the file name without the extension
 filename_pattern = re.compile("^(.+)\.xml$")
@@ -38,21 +39,31 @@ for input_filename in os.listdir(input_dir):
     # get the output file name (test1.xml -> test1.html)
     output_filename = filename_pattern.findall(input_filename)[0] + ".html"
 
-    # get the expected output
-    f = open(os.path.join(output_dir, output_filename), "r")
-    expected_output = f.read()
-
-    # exit with failed status if they are not equal
-    if test_output != expected_output:
-        print "Test " + input_filename + " failed :("
-        print "-- Output -------------\n" + test_output
-        print "-- Expected output ----\n" + expected_output
-        sys.exit(1)
-
-    # otherwise incremant the test counter
+    # check there is an expected output
+    try:
+        f = open(os.path.join(output_dir, output_filename), "r")
+    except:
+        print "Skipping " + input_filename + " as it does not have an expected output"
+        skipped_tests += 1
+        continue
     else:
-        test_count += 1
+        # get the expected output
+        expected_output = f.read()
+
+        # exit with failed status if they are not equal
+        if test_output != expected_output:
+            print "Test " + input_filename + " failed :("
+            print "-- Output -------------\n" + test_output
+            print "-- Expected output ----\n" + expected_output
+            sys.exit(1)
+
+        # otherwise incremant the test counter
+        else:
+            passed_tests += 1
 
 # exit with passed status
-print "All " + str(test_count) + " tests passed :)"
+if skipped_tests == 0:
+    print "All " + str(passed_tests) + " tests passed :)"
+else:
+    print str(passed_tests) + " tests passed and " + str(skipped_tests) + " tests skipped"
 sys.exit(0)
