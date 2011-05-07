42dcd3ceee202fe5674cd8a6f88ddff693b251b8
Elliot Goodrich <elliotgoodrich@gmail.com>
2011-05-07 19:51:53 +0100

    Renamed variables in test script to use underscores

--- tests/runtests.py
+++ tests/runtests.py
@@ -9,50 +9,50 @@ import sys
 from lxml import etree
 
 # directories and files
-inputdir = "input"
-outputdir = "output"
-XSLTfile = "../deXify.xslt"
+input_dir = "input"
+output_dir = "output"
+deXify_file = "../deXify.xslt"
 
 # test counter
-testcount = 0
+test_count = 0
 
 # load the regex to get the file name without the extension
-pattern = re.compile("^(.+)\.xml$")
+filename_pattern = re.compile("^(.+)\.xml$")
 
 # load the XSLT
-f = open(XSLTfile, "r")
-xsltdoc = etree.parse(f)
-transform = etree.XSLT(xsltdoc)
+f = open(deXify_file, "r")
+deXify_doc = etree.parse(f)
+deXify_transform = etree.XSLT(deXify_doc)
 
 # loop through each test file
-for inputfilename in os.listdir(inputdir):
+for input_filename in os.listdir(input_dir):
 
     # open the file
-    f = open(os.path.join(inputdir, inputfilename), "r")
-    testinput = etree.fromstring(f.read())
+    f = open(os.path.join(input_dir, input_filename), "r")
+    test_input = etree.fromstring(f.read())
 
     # perform the XSLT and get the output
-    result = transform(testinput)
-    testoutput = unicode(result).strip()
+    result = deXify_transform(test_input)
+    test_output = unicode(result).strip()
 
     # get the output file name (test1.xml -> test1.html)
-    outputfilename = pattern.findall(inputfilename)[0] + ".html"
+    output_filename = filename_pattern.findall(input_filename)[0] + ".html"
 
     # get the expected output
-    f = open(os.path.join(outputdir, outputfilename), "r")
-    expectedoutput = f.read().strip()
+    f = open(os.path.join(output_dir, output_filename), "r")
+    expected_output = f.read()
 
     # exit with failed status if they are not equal
-    if testoutput != expectedoutput:
-        print "Test " + inputfilename + " failed :("
-        print "-- Output -------------\n" + testoutput
-        print "-- Expected output ----\n" + expectedoutput
+    if test_output != expected_output:
+        print "Test " + input_filename + " failed :("
+        print "-- Output -------------\n" + test_output
+        print "-- Expected output ----\n" + expected_output
         sys.exit(1)
 
     # otherwise incremant the test counter
     else:
-        testcount += 1
+        test_count += 1
 
 # exit with passed status
-print "All " + str(testcount) + " tests passed :)"
+print "All " + str(test_count) + " tests passed :)"
 sys.exit(0)
